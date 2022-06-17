958e3a3d5069e79ca2b2673f82ec94246afd4f05
Vega Chen <vegachen@chancetop.com>
2022-06-17 15:33:23 +0800

    add Android showDialog function

--- android/src/main/java/com/rnvega/RNVegaModule.java
+++ android/src/main/java/com/rnvega/RNVegaModule.java
@@ -3,8 +3,10 @@ package com.rnvega;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Callback;
 
+import android.app.AlertDialog;
 import android.widget.Toast;
 
 public class RNVegaModule extends ReactContextBaseJavaModule {
@@ -26,4 +28,28 @@ public class RNVegaModule extends ReactContextBaseJavaModule {
         Toast.makeText(getReactApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
     }
 
+    @ReactMethod
+    public void showDialog(ReadableMap options, Callback positiveCB, Callback negativeCB) {
+        DialogParams dialogParams = new DialogParams();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getCurrentActivity());
+        if (options.hasKey(dialogParams.title)) {
+            builder.setTitle(options.getString(dialogParams.title));
+        }
+        if (options.hasKey(dialogParams.message)) {
+            builder.setMessage(options.getString(dialogParams.message));
+        }
+        if (options.hasKey(dialogParams.positive)) {
+            builder.setPositiveButton(options.getString(dialogParams.positive), (dialog, which) -> positiveCB.invoke());
+        }
+        if (options.hasKey(dialogParams.negative)) {
+            builder.setNegativeButton(options.getString(dialogParams.negative), ((dialog, which) -> negativeCB.invoke()));
+        }
+        builder.create().show();
+    }
+    private class DialogParams {
+        String title = "title";
+        String message = "message";
+        String positive = "positive";
+        String negative = "negative";
+    }
 }
--- src/nativeModules/RNVega.tsx
+++ src/nativeModules/RNVega.tsx
@@ -1,9 +1,34 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform } from "react-native";
 
-type RNVegaT = {
+interface DialogOptionsI {
+	title?: string;
+	message?: string;
+	positive?: string;
+	negative?: string;
+}
+
+export type RNVegaT = {
 	sayHello: () => void;
+	showDialog: (
+		options: DialogOptionsI,
+		positiveCB: () => void,
+		negativeCB: () => void
+	) => void;
 };
 
-const RNVega: RNVegaT = NativeModules.RNVega;
+const RNVegaModules = NativeModules.RNVega;
+
+const RNVega: RNVegaT = {
+	sayHello: () => {
+		if (Platform.OS === "android") {
+			RNVegaModules.sayHello();
+		}
+	},
+	showDialog: (...params) => {
+		if (Platform.OS === "android") {
+			RNVegaModules.showDialog(...params);
+		}
+	},
+};
 
 export default RNVega;
